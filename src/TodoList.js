76f57597494f47c2a4f8fad6016fa9d9870a3111
jayasri <jayasri.namburi@symphonize.com>
2023-10-13 19:27:12 +0530

    firstcommit

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,10 +4,14 @@ import React,{useState} from 'react';
 const TodoList=()=>{
     const[inputTask,setInputTask]=useState('');
     const[list,setList]=useState([]);
+    const [selectedDate, setSelectedDate] = useState(''); // Initialize selectedDate state
+  const [selectedTime, setSelectedTime] = useState(''); // Initialize selectedTime state
     const handleAddTodo=()=>{
         const newTask={
            id:Math.random(),
-            todo: inputTask
+            todo: inputTask,
+            date:selectedDate,
+            time:selectedTime,
         };
         setList([...list, newTask]);
         setInputTask('');
@@ -26,17 +30,25 @@ const TodoList=()=>{
             <h1>My To-Do-List</h1>
             <div className="Top">
                 <input className="input" type="text" value={inputTask} onChange={handleInputChange} placeholder="Enter a task" />
-                <button className="btn" onClick={handleAddTodo}>ADD</button>
+                <input type="date" value={selectedDate} onChange={(e) => setSelectedDate(e.target.value)}/>
+<input type="time"  value={selectedTime}  onChange={(e) => setSelectedTime(e.target.value)}/>
+
+ <button className="btn" onClick={handleAddTodo}>ADD</button>
             </div>
             <ul>
-                {
-                    list.map((todo)=>(
-                        <li className="task" key={todo.id}>
-                            {todo.todo}
-                            <button onClick={()=>handledeleteTodo(todo.id)}>
-                                Delete
-                            </button>
-                        </li>
+        {
+          list.map((todo) => (
+            <li className="task" key={todo.id}>
+    <div>
+        <strong>Date:</strong> {todo.date}<br />
+        <strong>Time:</strong> {todo.time}<br />
+        <strong>Task:</strong> {todo.todo}
+    </div>
+    <button onClick={() => handledeleteTodo(todo.id)}>
+        Delete
+    </button>
+</li>
+                    
                     ))}
                     
                 
